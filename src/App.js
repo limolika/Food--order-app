db91cec6aaf52751fb47d99388195b1e9acd843e
limolika <limolike@mail.ru>
2022-02-15 11:26:28 +0500

    added cart features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import CartProvider from './store/CartProvider'
-=======
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import CartProvider from './store/CartProvider'
 import Cart from './components/Cart/Cart'
->>>>>>> feat/cart
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
@@ -22,7 +19,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
